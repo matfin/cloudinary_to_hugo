eed3ad50786454b6d63cf58f222c585c981b8463
Matt Finucane <matfin@gmail.com>
2017-06-09 14:53:39 +0200

    Adding skeleton files for this small project.

---
+++ config.sample.js
@@ -0,0 +1,6 @@
+module.exports = {
+	cl_url: 		`api.cloudinary.com/v1_1/`,		
+	cl_cloud_name: 	`<cloud-name>`,
+	cl_key: 		`<key>`,
+	cl_secret: 		`<secret>`
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+const 	request = require('request'),
+		fs		= require('fs'),
+		config	= require('config');
+
+const template = (file) => {
+	return new Promise((resolve, reject) => {
+
+	});
+};
+
+console.log(api_params);
